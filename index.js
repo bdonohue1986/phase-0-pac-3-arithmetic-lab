798969d87529c415b9866080815fd1841ae0069d
Byron <byrondonohue@gmail.com>
2022-03-23 12:51:44 -0400

    commit tree 39ed16dc6d6ba381603a06df40af14baa61409d3

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+return a + b
+}
+
+function subtract(a, b) {
+return a - b
+}
+
+function multiply(a, b) {
+return a * b}
+
+function divide(a, b) {
+return a / b
+}
+
+let number = 10
+
+function add3() {
+    return (number += 3)
+}
+
+function subtract2() {
+    return (number -= 2)
+}
+
+function multiply5() {
+    return (number *= 5)
+}
+
+function divide1() {
+    return (number /= 1)
+}
+
+let n = 10
+
+function increment(n) {
+    return (n + 1)
+}
+function decrement(n) {
+    return (n - 1)
+}
+function makeInt(n) {
+    return (n * 1)
+}
